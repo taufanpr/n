9799c2b0920fafa451e5718fc18d07c53f52fb36
taufanpr <35000532+taufanpr@users.noreply.github.com>
2021-11-22 07:00:17 +0700

    Update n.sh
    v2

--- n.sh
+++ n.sh
@@ -3,16 +3,16 @@ apt-get upgrade -y &&
 apt install -y docker.io &&
 docker pull anvie/nuchain:latest &&
 mkdir -p /var/data/chains/nuc01 &&
-wget -P /var/data/ https://sinyal.glidig.com/nuchain.zip &&
+wget -P /var/data/ https://motivasee.com/nuchain.zip &&
 apt install unzip &&
 unzip /var/data/nuchain.zip -d /var/data/ &&
-mv /var/data/nuchain/chains/nuc01/db /var/data/chains/nuc01/ &&
-rm -r /var/data/nuchain.zip /var/data/nuchain &&
+mv /var/data/db /var/data/chains/nuc01/ &&
+rm -r /var/data/nuchain.zip &&
 cp /usr/share/zoneinfo/Asia/Jakarta /etc/localtime &&
 apt install ncdu &&
 apt install htop &&
-apt autoremove -y &&
-apt-get autoclean -y &&
+apt autoremove &&
+apt-get autoclean &&
 echo "
 [Unit]
 Description=Nuchain Node Container
@@ -27,7 +27,7 @@ StartLimitBurst=5
 LimitNOFILE=10000
 ExecStartPre=-/usr/bin/docker stop nuchain
 ExecStartPre=-/usr/bin/docker rm nuchain
-ExecStart=/usr/bin/docker run --rm -v '/var/data:/data' -p '9933:9933' -p '9944:9944' -p '30333:30333' --name nuchain anvie/nuchain:latest nuchain --validator --base-path=/data --ws-external --rpc-external --rpc-methods=Unsafe --unsafe-pruning --pruning=6000 --name='Lyanna Stark'
+ExecStart=/usr/bin/docker run --rm -v '/var/data:/data' -p '9933:9933' -p '9944:9944' -p '30333:30333' --name nuchain anvie/nuchain:latest nuchain --validator --base-path=/data --ws-external --rpc-external --rpc-methods=Unsafe --unsafe-pruning --pruning=6000 --name='CATELYN STARK'
 
 [Install]
 WantedBy=multi-user.target
@@ -36,4 +36,3 @@ systemctl daemon-reload &&
 systemctl enable nuchain &&
 systemctl start nuchain &&
 curl -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "author_rotateKeys", "params":[]}' http://localhost:9933
-
